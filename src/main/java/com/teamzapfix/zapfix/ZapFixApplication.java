f05fd63b5ff723a826fcaa0f909b5bf86318ce31
Yamil Daza <dazayamil07@gmail.com>
2025-07-01 21:03:18 -0300

    feat: initial ZapFix project setup

---
+++ src/main/java/com/teamzapfix/zapfix/ZapFixApplication.java
@@ -0,0 +1,13 @@
+package com.teamzapfix.zapfix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapFixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapFixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamzapfix/zapfix/ZapFixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamzapfix.zapfix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapFixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
